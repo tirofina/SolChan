29db2e21147fcd204a8bcd750816c926cb7aab18
eunsolAn <44772184+eunsolAn@users.noreply.github.com>
2018-11-05 23:05:53 +0900

    Add files via upload

---
+++ Membership.cpp
@@ -0,0 +1,5 @@
+#include"Membership.h"
+
+Membership::Membership(string name) {} //setName;
+string Membership::getName() {}
+void Membership::operator+(int x) { this->stamp += x; }//stamp_increase
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include "Membership.h"
+
+using namespace std;
+
+int main(int argc,char *argv[]) {
+	
+	//default_exe
+
+
+}
