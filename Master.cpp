221ce04beceeec581473e203b9f24a0aaa155b07
tiropine <ckscks1866@gmail.com>
2018-11-07 04:07:46 +0900

    Update Master.cpp

--- Master.cpp
+++ Master.cpp
@@ -1,6 +1,18 @@
 #include"Master.h"
+#include"Shared.h"
 
 Master::Master() {}
 int Master::input_pw() {}
 void Master::set_pw(int pw) {}
 void Master::check_pw(int pw, int input_pw) {}
+
+Material::Material() {}
+void Material::set_meterial(string name, int price, int amount) {} 
+void Material::order_meterial(string name, int amount) {} 
+void Material::print_meterial() {} 
+
+int Purchase::get_revenue() {}
+int Purchase::get_cost() {}
+void Purchase::set_revenue(int price) {}
+void Purchase::set_cost(int price) {}
+int Purchase::calculation_income() {}
