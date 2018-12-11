02a780ccd194715d9389e06431a666c8ad5ef066
tiropine <ckscks1866@gmail.com>
2018-12-11 16:57:39 +0900

    last

--- 1_final/Admin.cpp
+++
@@ -1,9 +0,0 @@
-#include "Admin.h"
-
-Admin::Admin(int max=5) {}
-void Admin::print_menu() { }
-void Admin::save_income(int val) {}
-int Admin::get_income() {}
-
-Admin* set_Admin() {} //set_Admin(Master* master)
-void check_menu() {} //Master* check_menu()
